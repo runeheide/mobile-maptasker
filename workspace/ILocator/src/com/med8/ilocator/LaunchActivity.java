005f0e61e19cfb281327e0810545b07c1199523c
nielsen.martinh@gmail.com <nielsen.martinh@gmail.com@d404d63f-a336-5aa1-733e-5aca368369e2>
2012-04-20 12:14:17 +0000

    ilocator version 1

---
+++ workspace/ILocator/src/com/med8/ilocator/LaunchActivity.java
@@ -0,0 +1,34 @@
+package com.med8.ilocator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class LaunchActivity extends ILocatorActivity {
+	
+	
+	
+    public void onCreate(Bundle savedInstanceState) {
+        
+    	super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch);
+        ImageView launchIcon = (ImageView)findViewById(R.id.ilocator_launch);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+    	launchIcon.startAnimation(fadeIn);
+    
+    	Handler x = new Handler();
+    	x.postDelayed(new LaunchHandler(), 8500);
+    	
+    	
+    }
+    
+    class LaunchHandler implements Runnable {
+    	public void run() {
+    		startActivity(new Intent(getApplication(), StartScreenActivity.class));
+    		LaunchActivity.this.finish();
+    	}
+    }
+}
