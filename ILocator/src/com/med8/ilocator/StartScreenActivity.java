8d83c6d4faf08ec3e289cd50fb2f0473c84b3880
nielsen.martinh@gmail.com <nielsen.martinh@gmail.com@d404d63f-a336-5aa1-733e-5aca368369e2>
2012-04-20 14:06:02 +0000

    Martin branch - it is neat!

---
+++ ILocator/src/com/med8/ilocator/StartScreenActivity.java
@@ -0,0 +1,58 @@
+package com.med8.ilocator;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.med8.support.*;
+public class StartScreenActivity extends Activity {
+
+	Context inContext = this;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+
+		final EditText name = (EditText)findViewById(R.id.EditText_Name);
+		final EditText password = (EditText)findViewById(R.id.EditText_Password);
+		Button signupButton = (Button)findViewById(R.id.signUpButton);	
+
+
+		signupButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent signupIntent = new Intent(view.getContext(), SignupActivity.class);
+				startActivityForResult(signupIntent,0);				
+			}
+		});
+
+
+		//		if (txtReader.compareStrings(name.getText().toString()) && txtReader.compareStrings(password.getText().toString()))
+		//		{
+		//			signupButton.setOnClickListener(new View.OnClickListener() {
+		//				public void onClick(View view) {
+		//					Intent signupIntent = new Intent(view.getContext(), SignupActivity.class);
+		//					startActivityForResult(signupIntent,0);				
+		//				}
+		//			});
+		//		}		
+		Button loginButton = (Button)findViewById(R.id.loginButton);
+
+		loginButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				TxtReader txtReader = new TxtReader();
+				//System.out.println(name.getText().toString() + ", " + password.getText().toString());
+				if (txtReader.getContent2(inContext , name.getText().toString(), "Password", password.getText().toString()))
+						{
+							System.out.println("2: " + name.getText().toString() + ", " + password.getText().toString());
+							Intent dashboardIntent = new Intent(view.getContext(), DashboardActivity.class);
+							startActivityForResult(dashboardIntent,0);
+						}
+			}
+		});
+
+	}	
+}
