cb09003255efef49a650d13dcac35ba5338c7dd6
nielsen.martinh@gmail.com <nielsen.martinh@gmail.com@d404d63f-a336-5aa1-733e-5aca368369e2>
2012-05-07 14:08:04 +0000

    "Under construction" screen implemented

--- trunk/ILocator/src/com/med8/ilocator/TasksActivity.java
+++ trunk/ILocator/src/com/med8/ilocator/TasksActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TasksActivity extends Activity {
 	
@@ -14,7 +15,7 @@ public class TasksActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tasks);		
 		
-		Button backButton = (Button)findViewById(R.id.backButton);
+		ImageButton backButton = (ImageButton)findViewById(R.id.backButton);
 		backButton.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View view) {
