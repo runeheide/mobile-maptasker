d6952db186ecdfd525adeef682e23de7c0723e32
nielsen.martinh@gmail.com <nielsen.martinh@gmail.com@d404d63f-a336-5aa1-733e-5aca368369e2>
2012-05-15 07:31:55 +0000

    ARApplicationActivity - major cleanups

--- ILocator/src/com/med8/ilocator/ARApplicationActivity.java
+++ ILocator/src/com/med8/ilocator/ARApplicationActivity.java
@@ -1,43 +1,17 @@
 package com.med8.ilocator;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
 import com.med8.ilocator.R;
 import com.med8.ilocator.augmentedreality.activity.AugmentedReality;
 import com.med8.ilocator.augmentedreality.data.ARData;
-import com.med8.ilocator.augmentedreality.data.BuzzDataSource;
 import com.med8.ilocator.augmentedreality.data.LocalDataSource;
-import com.med8.ilocator.augmentedreality.data.NetworkDataSource;
-import com.med8.ilocator.augmentedreality.data.TwitterDataSource;
-import com.med8.ilocator.augmentedreality.data.WikipediaDataSource;
 import com.med8.ilocator.augmentedreality.ui.Marker;
-import com.med8.support.TxtReader;
 import com.med8.support.TxtWriter;
 
 
@@ -48,12 +22,12 @@ import com.med8.support.TxtWriter;
  * @author Justin Wetherell <phishman3579@gmail.com>
  */
 public class ARApplicationActivity extends AugmentedReality {
-	Context mContext;
+	//Context mContext;
 	private static final String TAG = "ARApplication";
-	private static final String locale = Locale.getDefault().getLanguage();
-	private static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(1);
-	private static final ThreadPoolExecutor exeService = new ThreadPoolExecutor(1, 1, 20, TimeUnit.SECONDS, queue);
-	private static final Map<String,NetworkDataSource> sources = new ConcurrentHashMap<String,NetworkDataSource>();    
+	//private static final String locale = Locale.getDefault().getLanguage();
+	//private static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(1);
+	//private static final ThreadPoolExecutor exeService = new ThreadPoolExecutor(1, 1, 20, TimeUnit.SECONDS, queue);
+	//private static final Map<String,NetworkDataSource> sources = new ConcurrentHashMap<String,NetworkDataSource>();    
 	//private AlertDialog.Builder builder;
 
 	private LocalDataSource localData;
@@ -63,12 +37,7 @@ public class ARApplicationActivity extends AugmentedReality {
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
-
-	//	localData = new LocalDataSource(this.getResources());
-	//	ARData.addMarkers(localData.getMarkers());
-
     }
 
 	@Override
@@ -82,7 +51,7 @@ public class ARApplicationActivity extends AugmentedReality {
 
 	//   @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.v(TAG, "onOptionsItemSelected() item="+item);
+		//Log.v(TAG, "onOptionsItemSelected() item="+item);
 		switch (item.getItemId()) {
 		case R.id.showRadar:
 			showRadar = !showRadar;
@@ -106,10 +75,8 @@ public class ARApplicationActivity extends AugmentedReality {
 	@Override
 	public void onLocationChanged(Location location) {
 		super.onLocationChanged(location);
-
-		//System.out.println("YOUOUO" + location.getLatitude() + "," + location.getLongitude() +","+location.getAltitude());
+		
 		updateData(location.getLatitude(),location.getLongitude(),location.getAltitude());
-		//System.out.println("HEY MARTIN");
 	}
 
 	/**
@@ -123,95 +90,10 @@ public class ARApplicationActivity extends AugmentedReality {
 		
 		Intent intent = new Intent();
 		intent.setClass(this, AlertDialogViewActivity.class);
-		this.startActivity(intent);
-	
-/*		
-		final AlertDialog builder = new AlertDialog.Builder(this).create();
-		builder.setTitle(marker.getName());
-
-		builder.setMessage("Hydrant: type 1");
-		builder.setIcon(android.R.drawable.ic_dialog_alert);
-		builder.setButton("Done", new DialogInterface.OnClickListener() {
-
-			public void onClick(DialogInterface dialog, int which) {
-				//...
-			}
-		});
-		builder.setButton("OK", new DialogInterface.OnClickListener() {
-
-			public void onClick(DialogInterface dialog, int which) {
-				TxtWriter txtWriter = new TxtWriter();
-				txtWriter.writeEditObject(marker.getName(), "EventStatus", "OK"); 
-				//ARData.removeMarkers();
-				updateDataOnClick();
-				
-			}
-		});
-		builder.setButton2("Broken down", new DialogInterface.OnClickListener() {
-
-			public void onClick(DialogInterface dialog, int which) {
-				TxtWriter txtWriter = new TxtWriter();
-				txtWriter.writeEditObject(marker.getName(), "EventStatus", "Broken Down");
-				//ARData.removeMarkers();
-				//ARData.addMarkers(localData.getMarkers());
-				updateDataOnClick();
-
-			}
-		});
-		builder.setButton3("Needs attention", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				TxtWriter txtWriter = new TxtWriter();
-				txtWriter.writeEditObject(marker.getName(), "EventStatus", "Needs Attention");
-				//ARData.removeMarkers();
-				updateDataOnClick();
-			}
-
-		});
-		
-				
-		/*builder.setOnCancelListener(new OnCancelListener() {
-
-			   public void onCancel(DialogInterface dialog) {
-			    // TODO Auto-generated method stub
-			    TextView txt=(TextView)findViewById(R.id.txt);
-			    txt.setText(txt.getText()+" the cancel listner invoked");
-			   }
-			  });
-		
-		builder.show();
-		//      Toast t = Toast.makeText(getApplicationContext(), marker.getName(), Toast.LENGTH_SHORT);
-		//      t.setGravity(Gravity.CENTER, 0, 0);
-		//      t.show();
-*/	
+		this.startActivity(intent);	
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	private void updateDataOnClick()
-	{
-
-		//localData.getMarkers();
-		ARData.removeMarkers();
-		//ARData.getMarkers();
-		//ARData.addMarkers(localData.getMarkers());
-
-//		onCreate(null);
-		
-//		Intent intent = this.getIntent();
-//		startActivity(intent);
-//		finish();
-		
-//		Intent intent = new Intent();
-//		intent.setClass(this, this.getClass());  
-	
-//		Intent intent = this.getIntent();
-//		this.startActivity(intent);
-//		this.finish();
- 
-	}
-	
-/*	public void onResume()
+	/*	public void onResume()
 	{		
 		Intent arIntent = new Intent(this, ARApplicationActivity.class);
 		//finish();
@@ -228,7 +110,7 @@ public class ARApplicationActivity extends AugmentedReality {
 		try {
 			ARData.removeMarkers();
 			ARData.addMarkers(localData.getMarkers());
-			exeService.execute(
+			/*exeService.execute(
 					new Runnable() {
 						public void run() {
 							for (NetworkDataSource source : sources.values())
@@ -236,34 +118,13 @@ public class ARApplicationActivity extends AugmentedReality {
 						}
 					}
 					);
-		} catch (RejectedExecutionException rej) {
+		*/} catch (RejectedExecutionException rej) {
 			Log.w(TAG, "Not running new download Runnable, queue is full.");
 		} catch (Exception e) {
 			Log.e(TAG, "Exception running download Runnable.",e);
 		}
 	}
-
-	private static boolean download(NetworkDataSource source, double lat, double lon, double alt) {
-		if (source==null) return false;
-
-		String url = null;
-		try {
-			url = source.createRequestURL(lat, lon, alt, ARData.getRadius(), locale);    	
-		} catch (NullPointerException e) {
-			return false;
-		}
-
-		List<Marker> markers = null;
-		try {
-			markers = source.parse(url);
-		} catch (NullPointerException e) {
-			return false;
-		}
-
-		ARData.addMarkers(markers);
-		return true;
-	}	
-
+	
 	@Override
 	public void onResume()
 	{
